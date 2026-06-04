ec59a77b13283c51c161dd4190e1bc3aea218e28
zahra3bakr <zahraabobakr3@gmail.com>
2026-06-05 02:31:25 +0300

    Add fallback to Brevo API for email sending on Railway

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,6 +5,6 @@ const authController = require("../controllers/authController")
 router.post("/register", authController.register)
 router.post("/login", authController.login )
 router.post("/forgetPassword", authController.forgetPassword)
-router.put("/resetPassword/:token", authController.resetPassword)
+router.put("/resetPassword/:token", authController.resetPassword) // :token -> this what useParams() gets
 
 module.exports = router
