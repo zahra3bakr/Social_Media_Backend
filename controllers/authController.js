b5079e32164ef12bfb9c6ac7df11cca751d4c4b2
zahra3bakr <zahraabobakr3@gmail.com>
2026-06-05 22:22:17 +0300

    feat: return specific email error message instead of generic 500

--- controllers/authController.js
+++ controllers/authController.js
@@ -170,7 +170,11 @@ exports.forgetPassword = async (request , response) => {
             user.resetPasswordExpire = undefined;
             await user.save();
         }
-        response.status(500).json({ message: "Internal Server Error!" })
+        
+        response.status(500).json({ 
+            message: "Failed to send email. If you are on Railway, this is because Railway blocks Gmail (port 465). Please use Brevo API.",
+            errorDetails: error.message 
+        })
     }
 }
 
